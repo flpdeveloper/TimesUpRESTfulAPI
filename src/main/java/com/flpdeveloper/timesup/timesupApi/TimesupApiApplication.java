82faa13b164b00f4be975f3e1c44f1cf24e584c0
ferran <ferran.lopezpuig55@gmail.com>
2020-06-14 16:18:05 +0200

    init project with spring initializer

---
+++ src/main/java/com/flpdeveloper/timesup/timesupApi/TimesupApiApplication.java
@@ -0,0 +1,13 @@
+package com.flpdeveloper.timesup.timesupApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesupApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesupApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flpdeveloper/timesup/timesupApi/TimesupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flpdeveloper.timesup.timesupApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesupApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
